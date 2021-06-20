e094a9e6695b74788b75320aa273d1b6a56dd613
ty7575au <ty7575au@yahoo.co.jp>
2021-06-20 12:19:26 +0900

    complete

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
 	mode: 'development',
-	devtool: 'inline-source-map',
+	devtool: 'eval-source-map',
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'docs'),
